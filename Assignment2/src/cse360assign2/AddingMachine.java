1ee862cc2289fef255b3c3bd37f74ecd07721821
Christopher <star0chris@gmail.com>
2019-10-07 18:37:29 -0700

    Part 1: Initial Version

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,30 @@
+package cse360assign2;
+
+public class AddingMachine { //this program simulates an adding machine
+
+	private int total;
+	
+	public AddingMachine (){
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal (){ //this method gets the total and returns the value
+		return 0;  
+	}
+	
+	public void add (int value){ //adds a number 
+		
+	}
+	
+	public void subtract (int value){ //subtracts a number
+		
+	}
+		
+	public String toString (){ //returns the string representation of an object
+		return "";
+	}
+
+	public void clear(){ //clears the machine
+	
+	}
+}
